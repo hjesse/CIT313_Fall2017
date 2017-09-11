ebf11553fadc02328819d7e3d00c0dc502e4be6d
hjesse <31389296+hjesse@users.noreply.github.com>
2017-09-10 22:49:41 -0400

    Add files via upload

---
+++ a1/webconfig.php
@@ -0,0 +1,10 @@
+<?php
+
+define('ABSOLUTE_PATH', '/home/hjesse/htdocs/a1/');
+
+define('URL_ROOT', 'http://corsair.cs.iupui.edu:21911/a1');
+//define('URL_ROOT', 'http://corsair.cs.iupui.edu:21911/CIT313/FA2017/a1');
+
+
+
+?> 
