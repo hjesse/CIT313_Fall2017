caf25c4e2250c40fd0ce2f370ffc0674c190d6f9
hjesse <31389296+hjesse@users.noreply.github.com>
2017-10-14 15:47:47 -0400

    Add files via upload

---
+++ a2/views/view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>Assignment 2</title>
+
+	</head>
+	<body>
+		<h1>This is the View, hopefully.</h1>
+		<?php
+		echo "My Name is ".$firstname." ".$lastname."<br/>";
+		echo "<ul>";
+		echo "<li>".$firstname."</li>"."<li>".$lastname."</li>"."<li>".$email."</li>"."<li>".$role."</li>".;
+		echo "</ul>";
+		?>
+
+	</body>	
+
+
+
+
+	</html>
